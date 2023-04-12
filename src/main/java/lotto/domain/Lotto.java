51de732a3fe1fe91423ee2539e2413683f9895cd
SEC <c3392302@naver.com>
2023-04-12 13:15:43 +0900

    feat: lotto의 validation 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,19 +1,16 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 import java.util.List;
+import lotto.validation.LottoTicketValidation;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoTicketValidation.validate(numbers);
         this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     // TODO: 추가 기능 구현
---
+++ src/main/java/lotto/validation/LottoTicketValidation.java
@@ -0,0 +1,34 @@
+package lotto.validation;
+
+import java.util.List;
+
+public class LottoTicketValidation {
+
+    public static void validate(List<Integer> numbers){
+        validateOriginalSize(numbers);
+        validateDistinctSize(numbers);
+        validateBetween1And45(numbers);
+
+    }
+
+    private static void validateOriginalSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또의 사이즈는 6이여야 합니다.");
+        }
+    }
+
+    private static void validateDistinctSize(List<Integer> numbers){
+        int count = (int) numbers.stream().distinct().count();
+        if(count != 6){
+            throw new IllegalArgumentException("[ERROR] 로또는 중복되지 않는 6개의 숫자입니다.");
+        }
+    }
+
+    private static void validateBetween1And45(List<Integer> numbers){
+        boolean flag = numbers.stream().allMatch(num -> num >= 1 && num <= 45);
+        if(flag != true){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이입니다.");
+        }
+    }
+
+}
