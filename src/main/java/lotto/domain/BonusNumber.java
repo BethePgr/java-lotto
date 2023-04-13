a8e8fa28ae505218427d503abd91266d181a986a
jongwonpc <c3392302@naver.com>
2023-04-14 05:51:12 +0900

    refactor: bonusNumber 리팩토링

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,13 +1,26 @@
 package lotto.domain;
 
+import java.util.List;
+import lotto.validation.BonusNumberValidation;
+
 public class BonusNumber {
 
     private final String bonusNumber;
+    private static final String ERROR_MESSAGE = "[ERROR] 1과 45 사이의 숫자여야합니다.";
 
-    public BonusNumber(String bonusNumber){
+    public BonusNumber(String bonusNumber,List<Integer> winNumber){
+        validateBonusNumber(bonusNumber,winNumber);
         this.bonusNumber = bonusNumber;
     }
 
+    private void validateBonusNumber(String number,List<Integer> winNumber){
+        try{
+            BonusNumberValidation.validate(number,winNumber);
+        }catch (IllegalArgumentException e){
+            System.out.println(ERROR_MESSAGE);
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
     public String getBonusNumber(){
         return bonusNumber;
     }
