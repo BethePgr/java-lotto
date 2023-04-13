efcfb2f3b03f4742796339e7e130c59e27ad1a6f
SEC <c3392302@naver.com>
2023-04-13 10:21:53 +0900

    test: 보너스 번호 예외처리 테스트 완료

---
+++ src/test/java/lotto/validation/BonusNumberValidationTest.java
@@ -0,0 +1,28 @@
+package lotto.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BonusNumberValidationTest {
+
+    @Test
+    void 에러_안뜸() throws Exception {
+        List<String> list = List.of("1", "2", "3", "4", "5", "6");
+        assertDoesNotThrow(()->BonusNumberValidation.validate("44",list));
+    }
+
+    @Test
+    void 에러_뜸() throws Exception {
+        List<String> list = List.of("1", "2", "3", "4", "5", "6");
+        assertThrows(IllegalArgumentException.class,()->BonusNumberValidation.validate("46",list));
+    }
+
+    @Test
+    void 에러_뜸1() throws Exception {
+        List<String> list = List.of("1", "2", "3", "4", "5", "6");
+        assertThrows(IllegalArgumentException.class,()->BonusNumberValidation.validate("4",list));
+    }
+
+}
