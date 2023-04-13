93f2d3ea091ce8b211b02389895d25d6a2a34c4b
jongwonpc <c3392302@naver.com>
2023-04-14 01:20:48 +0900

    test: Money validate 테스트 완료

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @Test
+    void 나머지가_있으므로_오류를_던진다() throws Exception{
+        assertThrows(IllegalArgumentException.class,() -> {
+            Money money = new Money("4001");
+        });
+    }
+
+    @Test
+    void 문자가_있으므로_오류를_던진다() throws Exception{
+        assertThrows(IllegalArgumentException.class,() -> {
+            Money money = new Money("a4000");
+        });
+    }
+
+    @Test
+    void 오류_없음() throws Exception{
+        assertDoesNotThrow(() -> {
+            Money money = new Money("4000");
+        });
+
+        assertDoesNotThrow(() -> {
+            Money money = new Money("41000");
+        });
+    }
+}
